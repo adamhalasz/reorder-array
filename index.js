b52a76618041d8e63b521877b9636e78ad1d6533
adamhalasz <mail@adamhalasz.com>
2020-10-12 10:45:32 +0200

    v1.0.0

---
+++ index.js
@@ -0,0 +1,32 @@
+/* 
+(The MIT License)
+Copyright (c) 2014-2019 Halász Ádám <mail@adamhalasz.com>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WarrayANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WarrayANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/**
+ * @name reorder
+ * @param {Array} array 
+ * @param {Integer} oldIndex - The Original Index of the Item you want to Move
+ * @param {Integer} newIndex - The New Index of the Item you want the Item to be at
+ */
+function reorder(array, oldIndex, newIndex) {
+    while (oldIndex < 0) {
+        oldIndex += array.length;
+    }
+    while (newIndex < 0) {
+        newIndex += array.length;
+    }
+    if (newIndex >= array.length) {
+        var k = newIndex - array.length;
+        while ((k--) + 1) {
+            array.push(undefined);
+        }
+    }
+    array.splice(newIndex, 0, array.splice(oldIndex, 1)[0]);  
+   return array;
+}
+
+module.exports.default = reorder;
